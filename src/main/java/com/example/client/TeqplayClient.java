baf964fa99ed0dc70b3c164e537118ff35bb6d14
funding-dev13 <funding-dev13@samakita.co.id>
2024-12-08 02:20:47 +0700

    feat: use virtual threads

--- src/main/java/com/example/client/TeqplayClient.java
+++ src/main/java/com/example/client/TeqplayClient.java
@@ -2,18 +2,22 @@ package com.example.client;
 
 import com.example.config.TeqplayConfiguration;
 import com.example.dto.response.TeqplayLoginResponse;
+import io.micronaut.core.annotation.Blocking;
 import io.micronaut.core.async.annotation.SingleResult;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.retry.annotation.Retryable;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import jakarta.validation.constraints.NotBlank;
-import org.reactivestreams.Publisher;
 
 @Client(id = TeqplayConfiguration.TEQPLAY_PREFIX)
 @Retryable
 public interface TeqplayClient {
 
   @Post("/auth/login")
+  @ExecuteOn(TaskExecutors.VIRTUAL)
+  @Blocking
   @SingleResult
-  Publisher<TeqplayLoginResponse> login(@NotBlank String username, @NotBlank String password);
+  TeqplayLoginResponse login(@NotBlank String username, @NotBlank String password);
 }
