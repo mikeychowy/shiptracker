d5d87fcb3e3a2017ebdc801bcffc86751683d397
funding-dev13 <funding-dev13@samakita.co.id>
2024-12-13 10:30:27 +0700

    feat: execute on blocking thread

--- src/main/java/com/example/controller/ShipController.java
+++ src/main/java/com/example/controller/ShipController.java
@@ -6,9 +6,13 @@ import com.example.dto.response.ShipResponse;
 import com.example.service.ShipDataService;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import jakarta.inject.Inject;
 import java.util.List;
 
@@ -22,12 +26,16 @@ public class ShipController {
   }
 
   @Get("/portEvents")
+  @ExecuteOn(TaskExecutors.BLOCKING)
+  @Produces(MediaType.APPLICATION_JSON)
   public HttpResponse<List<PortEventResponse>> getPortEvents(
       @QueryValue @Nullable PortEvent portEvent) {
     return HttpResponse.ok().body(shipDataService.findPortEvents(portEvent));
   }
 
   @Get("/inPort")
+  @ExecuteOn(TaskExecutors.BLOCKING)
+  @Produces(MediaType.APPLICATION_JSON)
   public HttpResponse<List<ShipResponse>> getShipsInPort() {
     return HttpResponse.ok().body(shipDataService.findAllShipsInPort());
   }
